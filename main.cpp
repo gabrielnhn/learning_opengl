c6378bfe7f5b312a26ed6e90d2dcb90e0adc7170
Gabriel Hishida <gabrielnhn@duck.com>
2025-02-24 00:36:08 +0100

    it doesnt make sense for me still

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp> 
 
-
+auto camera = glm::vec3(3, 2, 3);
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -18,6 +18,15 @@ void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        camera.x -= 0.1;
+        std::cout << "REPEAT A" << std::endl;
+    }
+
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.x += 0.1;
 }
 
 int main()
@@ -62,11 +71,9 @@ int main()
 
 
     //fuck it perspective
-    // glm::mat4 projection = glm::perspective(30.0f, 1.0f, 0.3f, 2.0f);
-
 
     glm::mat4 projection = glm::perspective(45.0f, 1.0f, 0.1f, 60.0f);
-    glm::mat4 view = glm::lookAt(glm::vec3(3, 2, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    glm::mat4 view = glm::lookAt(camera, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
 
     // glm::mat4 model = glm::mat4(1.0f);
@@ -77,13 +84,6 @@ int main()
 
     std::vector<glm::vec3> gl_vertices;
 
-    // for(int i = 0; i < vertices.size(); i++)
-    // {
-    //     // gl_vertices.push_back (glm::vec3(projection * vertices[i]) ); 
-    //     gl_vertices.push_back (glm::vec3(mvp * vertices[i]) ); 
-    // }
-
-
     unsigned int VBO;
     glGenBuffers(1, &VBO);
 
@@ -93,8 +93,6 @@ int main()
 
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);  
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(float), &vertices.front(), GL_DYNAMIC_DRAW);
     glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(glm::vec4), &vertices.front(), GL_DYNAMIC_DRAW);
 
 
@@ -119,7 +117,6 @@ int main()
         4,5,6,
         5,6,7,
     };
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size()*sizeof(int), &indices.front(), GL_DYNAMIC_DRAW); 
 
     const char *vertexShaderSourceGLSLCode = "#version 330 core\n"
@@ -128,7 +125,6 @@ int main()
         "uniform mat4 mvp;\n"
         "void main()\n"
         "{\n"
-
         // "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
         // "   gl_Position = mvp * vec4    (aPos, 1.0);\n"
         "   gl_Position = mvp * aPos;\n"
@@ -168,8 +164,10 @@ int main()
     glEnable(GL_DEPTH_TEST);
 
     // int i = 0;
-    float vx = 0.012;
-    float vy = 0.013;
+    // float vx = 0.012;
+    float vx = 0;
+    // float vy = 0.013;
+    float vy = 0;
     // bool goingright = true;
     int j = 0;
     while(!glfwWindowShouldClose(window))
@@ -195,7 +193,6 @@ int main()
 
             if (vertices[1].x > BOUND)
             {
-                // goingright = false;
                 vx = -vx;
                 glClearColor(0.3f, 0.5f, 0.8f, 1.0f);
 
@@ -203,7 +200,6 @@ int main()
 
             if (vertices[0].x < -BOUND)
             {
-                // goingright = true;
                 vx = -vx;
                 glClearColor(0.9f, 0.3f, 0.0f, 1.0f);
             }
@@ -213,27 +209,28 @@ int main()
                 vertices[k].x += vx;
                 vertices[k].y += vy;
              
-                std::cout << k << ": " << vertices[k].y << std::endl;
+                // std::cout << k << ": " << vertices[k].y << std::endl;
                 
             }
 
             glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            // glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(glm::vec4), &gl_vertices.front(), GL_DYNAMIC_DRAW);
-            // glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(float), &vertices[0], GL_DYNAMIC_DRAW);
             glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(glm::vec4), &vertices.front(), GL_DYNAMIC_DRAW);
 
         }
 
+        // remake projection
+        view = glm::lookAt(camera, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+        glm::mat4 mvp = projection * view * model;
+        int mvpLocation = glGetUniformLocation(shaderProgram, "mvp");
+        glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, glm::value_ptr(mvp));
+
+
         processInput(window);
 
-        // glClear(GL_COLOR_BUFFER_BIT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
-        // glBindVertexArray(VAO);
-        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0);
 
 
         glfwSwapBuffers(window);
